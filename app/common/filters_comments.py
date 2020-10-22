dd8486e44453ddc8302c0e1d86a13565a11d6e99
Anekdotin <edwin.eames214@gmail.com>
2020-10-21 20:24:41 -0400

    added red green arrows

--- app/common/filters_comments.py
+++ app/common/filters_comments.py
@@ -1,16 +1,30 @@
 from app import app
 
 
-@app.template_filter('subcommentsofcomment')
-def subcommentsofcomment(commentid):
+@app.template_filter('seeifvoted')
+def seeifvoted(postid):
     """
-    returns all subcomments of a comment in order of date posted...
+    see if user voted on a post
     """
-    from app.models import Comments
+    from app.models import PostUpvotes
     from app import db
+    from flask_login import current_user
 
-    getsubcomments = db.session.query(Comments)
-    getsubcomments = getsubcomments.filter(Comments.comment_parent_id ==commentid)
-    getsubcomments.all()
-    for f in getsubcomments:
-        yield f
+    if current_user.is_authenticated:
+        get_vote = db.session.query(PostUpvotes).filter(PostUpvotes.user_id == current_user.id,
+                                                        PostUpvotes.post_id == postid).first()
+
+        if get_vote:
+
+            if get_vote.vote_up == 1:
+                vote = 1
+            elif get_vote.vote_up == 0:
+                vote = 2
+            else:
+                vote = 0
+        else:
+            vote = 0
+    else:
+        vote = 0
+
+    return vote
